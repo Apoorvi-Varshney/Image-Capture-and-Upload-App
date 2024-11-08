f2521a4a1de6c982af41be2317e957abefdaa4b5
Apoorvi <apoorvi25@gmail.com>
2024-11-08 14:29:05 +0530

    Initial Commit

---
+++ ImageUpload/ImageUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageUploadApp.swift
+//  ImageUpload
+//
+//  Created by Apoorvi on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
